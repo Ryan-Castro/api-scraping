f6f6223951718891b9d405287222b0d09c3d5d4c
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2023-11-03 06:56:10 -0300

    Update aliExpress.js

--- src/pages/api/aliExpress.js
+++ src/pages/api/aliExpress.js
@@ -1,43 +1,45 @@
-import chromium from 'chrome-aws-lambda'
-
+const chromium = require('chrome-aws-lambda');
 
 
 export default async function handler(req,res) {
+  let result = {}
+  let browser = null
   if(req.query.link){
     try{
-      let browser = await chromium.puppeteer.launch({
+      browser = await chromium.puppeteer.launch({
         args: chromium.args,
         defaultViewport: chromium.defaultViewport,
         executablePath: await chromium.executablePath,
         headless: chromium.headless,
         ignoreHTTPSErrors: true,
       });
-      let _page = await browser.newPage()
-      await _page.goto(req.query.link)
-      const src_img = await _page.$eval('.slider--img--D7MJNPZ.slider--active--ffqmskh img', (img)=>{return img.src})
-      const title_product = await _page.$eval('.title--wrap--Ms9Zv4A h1', (title)=>{return title.innerText})
-      const base_price = await _page.$eval('.price--originalText--Zsc6sMv', (bPrice)=>{return bPrice.innerText.replace('R$', '')})
-      const discount_price = await _page.$$eval('.es--wrap--erdmPRe.notranslate span', (sPrices)=>{let stn = "";sPrices.map((sPrice)=>{stn += sPrice.innerText});return stn.replace("R$", "").replaceAll(" ", "")})
-      const discount_percentage = await _page.$eval('.price--discount--xET8qnP', (percentage)=>{return percentage.innerText.replaceAll('-', '').replaceAll('%', '')})
+      let page = await browser.newPage()
+      await page.goto(req.query.link)
+      const src_img = await page.$eval('.slider--img--D7MJNPZ.slider--active--ffqmskh img', (img)=>{return img.src})
+      const title_product = await page.$eval('.title--wrap--Ms9Zv4A h1', (title)=>{return title.innerText})
+      const base_price = await page.$eval('.price--originalText--Zsc6sMv', (bPrice)=>{return bPrice.innerText.replace('R$', '')})
+      const discount_price = await page.$$eval('.es--wrap--erdmPRe.notranslate span', (sPrices)=>{let stn = "";sPrices.map((sPrice)=>{stn += sPrice.innerText});return stn.replace("R$", "").replaceAll(" ", "")})
+      const discount_percentage = await page.$eval('.price--discount--xET8qnP', (percentage)=>{return percentage.innerText.replaceAll('-', '').replaceAll('%', '')})
       
-      await browser.close()
-      return res.status(200).send({
+      result = {
         'linkImg': src_img,
         'titleProcuct':title_product,
         'basePrice':base_price,
         'diccountPrice':discount_price,
         'discountPercentage':discount_percentage,
-      })
+      }
+      return res.send(result)
       } catch (error) {
-        return callback(error);
+        return res.send({'a': error})
       } finally {
         if (browser !== null) {
           await browser.close();
         }
       }
     } else {
-      return res.status(200).send({'Erro': 'Sem o link. Adicione o parametro link'})
-  }
+      result = {'Erro': 'Sem o link. Adicione o parametro link'}
+    }
+    return res.send(result)
 }
 
 
