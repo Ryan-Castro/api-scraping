c9936ffc1f2edf426b93a2d4a3e4a2c8712d07bd
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2023-11-02 22:33:48 -0300

    criando a primeira api

---
+++ src/pages/api/aliExpress.js
@@ -0,0 +1,36 @@
+import puppeteer, { Page } from 'puppeteer-core'
+import chrome from 'chrome-aws-lambda'
+
+
+
+export default async function handler(req,res) {
+  let options;
+  const chromeExecPaths = {
+    win32: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+  }
+  const exePath = chromeExecPaths[process.platform]
+  options = {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless
+  }
+  const browser = await puppeteer.launch(options)
+  let _page = await browser.newPage()
+  await _page.goto(req.query.link)
+  const src_img = await _page.$eval('.slider--img--D7MJNPZ.slider--active--ffqmskh img', (img)=>{return img.src})
+  const title_product = await _page.$eval('.title--wrap--Ms9Zv4A h1', (title)=>{return title.innerText})
+  const base_price = await _page.$eval('.price--originalText--Zsc6sMv', (bPrice)=>{return bPrice.innerText.replace('R$', '')})
+  const discount_price = await _page.$$eval('.es--wrap--erdmPRe.notranslate span', (sPrices)=>{let stn = "";sPrices.map((sPrice)=>{stn += sPrice.innerText});return stn.replace("R$", "").replaceAll(" ", "")})
+  const discount_percentage = await _page.$eval('.price--discount--xET8qnP', (percentage)=>{return percentage.innerText.replaceAll('-', '').replaceAll('%', '')})
+  
+  await browser.close()
+  return res.status(200).send({
+    'linkImg': src_img,
+    'titleProcuct':title_product,
+    'basePrice':base_price,
+    'diccountPrice':discount_price,
+    'discountPercentage':discount_percentage,
+  })
+}
+
+
