9583601cee7777a30d03e1101d72d6745b732fa9
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2023-11-03 09:19:48 -0300

    Update aliExpress.js

--- src/pages/api/aliExpress.js
+++ src/pages/api/aliExpress.js
@@ -9,19 +9,20 @@ export default async function handler(req,res) {
         args: Chromium.args,
         defaultViewport: Chromium.defaultViewport,
         executablePath: await Chromium.executablePath(),
+        //executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
         headless: Chromium.headless,
       });
     
       let page = await browser.newPage()
       await page.goto(req.query.link)
-      const src_img = await page.$eval('.slider--img--D7MJNPZ.slider--active--ffqmskh img', (img)=>{return img.src})
+      //const src_img = await page.$eval('.slider--img--D7MJNPZ.slider--active--ffqmskh img', (img)=>{return img.src})
       const title_product = await page.$eval('.title--wrap--Ms9Zv4A h1', (title)=>{return title.innerText})
       const base_price = await page.$eval('.price--originalText--Zsc6sMv', (bPrice)=>{return bPrice.innerText.replace('R$', '')})
       const discount_price = await page.$$eval('.es--wrap--erdmPRe.notranslate span', (sPrices)=>{let stn = "";sPrices.map((sPrice)=>{stn += sPrice.innerText});return stn.replace("R$", "").replaceAll(" ", "")})
       const discount_percentage = await page.$eval('.price--discount--xET8qnP', (percentage)=>{return percentage.innerText.replaceAll('-', '').replaceAll('%', '')})
       await browser.close();
       result = {
-        'linkImg': src_img,
+        //'linkImg': src_img,
         'titleProcuct':title_product,
         'basePrice':base_price,
         'diccountPrice':discount_price,
